8c97d453cb79a8563bac51b4d52ab1a571cdd3e9
Aldo796 <mo.alden25@gmail.com>
2021-12-11 23:25:35 -0500

    Added individual file error checker

--- Repack.py
+++ Repack.py
@@ -1,6 +1,7 @@
 import UnityPy
 import os
 import json
+import traceback
 
 
 #PathIDs inside Unity
@@ -106,88 +107,95 @@ for obj in env.objects:
             for trainer in tree[trainerPoke][1:]:
                 fp = os.path.join(trainerPoke, f"{trainer['ID']}.txt")
                 if os.path.isfile(fp):
-                    with open(fp, "r", encoding = "utf8") as f:
-                        team = f.read().rstrip("\n").split("\n")
-                    # print(team)
-                    breakpoints = [0]
-                    for i in range(len(team)):
-                        if team[i] == "":
-                            # print("added point " + str(i))
-                            breakpoints.append(i + 1)
-                        team[i] = team[i].strip()
-                    if len(breakpoints) > 1:
-                        breakpoints.append(len(team))
+                    try:
+                        with open(fp, "r", encoding = "utf8") as f:
+                            team = f.read().rstrip("\n").split("\n")
+                        # print(team)
+                        breakpoints = [0]
+                        for i in range(len(team)):
+                            if team[i] == "":
+                                # print("added point " + str(i))
+                                breakpoints.append(i + 1)
+                            team[i] = team[i].strip()
+                        if len(breakpoints) > 1:
+                            breakpoints.append(len(team))
+                                
+                        # print(breakpoints)
+                        pokeNum = 0
+                        for point in range(len(breakpoints) - 1):
+                            pokeNum += 1
                             
-                    # print(breakpoints)
-                    pokeNum = 0
-                    for point in range(len(breakpoints) - 1):
-                        pokeNum += 1
-                        
-                        ##Name and Ability are always present so we don't need to perform checks
-                        firstLine = team[breakpoints[point]]
-                        secondLine = team[breakpoints[point] + 1]
-                        # print(firstLine)
-                        # print(secondLine)
-                        try:
-                            trainer["P"f"{pokeNum}MonsNo"] = name(firstLine.split(" ")[0].upper())
-                        except:
-                            ##Pokemon Like Mr. Mime exist with a space in between AND IT RUINS EVERYTHING AAAAAAAAAAAAAAA
+                            ##Name and Ability are always present so we don't need to perform checks
+                            firstLine = team[breakpoints[point]]
+                            secondLine = team[breakpoints[point] + 1]
+                            # print(firstLine)
+                            # print(secondLine)
+                            try:
+                                trainer["P"f"{pokeNum}MonsNo"] = name(firstLine.split(" ")[0].upper())
+                            except:
+                                ##Pokemon Like Mr. Mime exist with a space in between AND IT RUINS EVERYTHING AAAAAAAAAAAAAAA
+                                
+                                trainer["P"f"{pokeNum}MonsNo"] = name(firstLine.split(" ")[0].upper() + " " + firstLine.split(" ")[1].upper())
+                            ##Gender
+                            if "(" in firstLine:
+                                index = firstLine.index("(")
+                                trainer["P"f"{pokeNum}Sex"] = genderList.index(firstLine[index:index + 3].upper())
+                            ##Item
+                            if "@" in firstLine:
+                                index = firstLine.index("@")
+                                trainer["P"f"{pokeNum}Item"] = itemList.index(firstLine[index + 1:].strip().upper())
+                                
+                            ##Ability
+                            trainer["P"f"{pokeNum}Tokusei"] = ability(secondLine.upper())
                             
-                            trainer["P"f"{pokeNum}MonsNo"] = name(firstLine.split(" ")[0].upper() + " " + firstLine.split(" ")[1].upper())
-                        ##Gender
-                        if "(" in firstLine:
-                            index = firstLine.index("(")
-                            trainer["P"f"{pokeNum}Sex"] = genderList.index(firstLine[index:index + 3].upper())
-                        ##Item
-                        if "@" in firstLine:
-                            index = firstLine.index("@")
-                            trainer["P"f"{pokeNum}Item"] = itemList.index(firstLine[index + 1:].strip().upper())
+                            if "Level:" not in team[breakpoints[point] + 2]:
+                                trainer["P"f"{pokeNum}Level"] = 100 ##Level is 100 if not shown
                             
-                        ##Ability
-                        trainer["P"f"{pokeNum}Tokusei"] = ability(secondLine.upper())
-                        
-                        if "Level:" not in team[breakpoints[point] + 2]:
-                            trainer["P"f"{pokeNum}Level"] = 100 ##Level is 100 if not shown
-                        
-                        moveNum = 0
-                        for line in range(breakpoints[point] + 2, breakpoints[1]):
-                            # Pokemon Showdown Format
-                            # PokemonName (Gender) @ HeldItem
-                            # Ability: AbilityName
-                            # Level: Level
-                            # Shiny: Y/N
-                            # EVs: x Hp / x Atk / etc.
-                            # NatureName Nature
-                            # IVs: x Hp / x Atk / etc.
-                            # - Move1
-                            # - Move2
-                            # - Move3
-                            # - Move4
-                            ##Move
-                            pokemon = team[line].upper()
-                            if "-" in pokemon:
-                                moveNum += 1
-                                trainer["P"f"{pokeNum}Waza"f"{moveNum}"] = moveList.index(pokemon.strip(" -"))
-                                
-                            elif "NATURE" in pokemon:
-                                trainer["P"f"{pokeNum}Seikaku"] = natureList.index(pokemon.split(" ")[0])
-                                
-                            elif "SHINY:" in pokemon:
-                                ##Shiny should only be present if it is, so not gonna bother checking Shiny: N
-                                trainer["P"f"{pokeNum}IsRare"] = 1
-                                
-                            elif "LEVEL:" in pokemon:
-                                trainer["P"f"{pokeNum}Level"] = int(pokemon.split(" ")[1])
-                                
-                            elif "EVS:" in pokemon:
-                                EVdic = formatStat(pokemon[4:].split("/"))
-                                for key in EVdic.keys():
-                                    trainer["P"f"{pokeNum}Effort{key}"] = EVdic[key]
+                            moveNum = 0
+                            for line in range(breakpoints[point] + 2, breakpoints[1]):
+                                # Pokemon Showdown Format
+                                # PokemonName (Gender) @ HeldItem
+                                # Ability: AbilityName
+                                # Level: Level
+                                # Shiny: Y/N
+                                # EVs: x Hp / x Atk / etc.
+                                # NatureName Nature
+                                # IVs: x Hp / x Atk / etc.
+                                # - Move1
+                                # - Move2
+                                # - Move3
+                                # - Move4
+                                ##Move
+                                pokemon = team[line].upper()
+                                if "-" in pokemon:
+                                    moveNum += 1
+                                    trainer["P"f"{pokeNum}Waza"f"{moveNum}"] = moveList.index(pokemon.strip(" -"))
                                     
-                            elif "IVS"  in pokemon:
-                                IVdic = formatStat(pokemon[4:].split("/"))
-                                for key in IVdic.keys():
-                                    trainer["P"f"{pokeNum}Talent{key}"] = IVdic[key]
+                                elif "NATURE" in pokemon:
+                                    trainer["P"f"{pokeNum}Seikaku"] = natureList.index(pokemon.split(" ")[0])
+                                    
+                                elif "SHINY:" in pokemon:
+                                    ##Shiny should only be present if it is, so not gonna bother checking Shiny: N
+                                    trainer["P"f"{pokeNum}IsRare"] = 1
+                                    
+                                elif "LEVEL:" in pokemon:
+                                    trainer["P"f"{pokeNum}Level"] = int(pokemon.split(" ")[1])
+                                    
+                                elif "EVS:" in pokemon:
+                                    EVdic = formatStat(pokemon[4:].split("/"))
+                                    for key in EVdic.keys():
+                                        trainer["P"f"{pokeNum}Effort{key}"] = EVdic[key]
+                                        
+                                elif "IVS"  in pokemon:
+                                    IVdic = formatStat(pokemon[4:].split("/"))
+                                    for key in IVdic.keys():
+                                        trainer["P"f"{pokeNum}Talent{key}"] = IVdic[key]
+                                        
+                    except:
+                        print("An Error has occured while packing "f"{fp}")
+                        print(traceback.format_exc())
+                                            
+                                        
                 else:
                     print("Error "f"{fp} not found")                     
                                     
